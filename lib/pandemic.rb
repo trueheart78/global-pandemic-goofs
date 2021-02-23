a723a309c709f6c4ce0732fa7b676d514716d604
Josh Mills <josh@trueheart78.com>
2021-02-23 17:45:28 -0500

    First pass at getting the Pandemic lib sorted

--- lib/pandemic.rb
+++ lib/pandemic.rb
@@ -2,6 +2,70 @@
 
 class Pandemic
   def self.goof
-    ['global', 'paladin', 'armory'].join ' '
+    ['global', [self.concoction, self.business].sample].join ' '
+  end
+
+  class << self
+    def concoction
+      [ self.p_word, self.place ]
+    end
+
+    def p_word
+      [
+        'paladin',
+        'pizza',
+        'paparazzi',
+        'penguin',
+        'pipe',
+        'pygmy',
+        'pineapple',
+        'panini',
+        'paper clip',
+        'pinto bean',
+        'package',
+        'page',
+        'paper',
+        'picture',
+        'photo',
+        'plant',
+        'paddlefish'
+      ].sample
+    end
+
+    def place
+      [
+        'training camp',
+        'armory',
+        'factory',
+        'warehouse',
+        'school',
+        'office',
+        'law firm',
+        'library',
+        'study hall',
+        'bed',
+        'living room',
+        'van down by the river',
+        'concert hall',
+        'phylactery',
+        'express',
+        'restaurant',
+        'park',
+        'path',
+        'plant'
+      ].sample
+    end
+
+    def business
+      [
+        'Panda Express',
+        'Papa John\'s',
+        'Pizza Hut',
+        'Penn Station',
+        'Priceline.com',
+        'Petsmart',
+        'P.F. Chang\'s'
+      ].sample
+    end
   end
 end
