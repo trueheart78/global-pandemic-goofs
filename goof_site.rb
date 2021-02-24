5cbe3ef36bbf328388e96a96a9d5a3cafc9ef910
Josh Mills <josh@trueheart78.com>
2021-02-24 13:44:03 -0500

    New favicon - microbe

--- goof_site.rb
+++ goof_site.rb
@@ -8,16 +8,19 @@ class GoofSite < Sinatra::Base
      <html>
        <head>
          <title>Pandemic Goofs</title>
+         <link rel='shortcut icon' type='image/png' href='icons/microbe.png'/>
        </head>
        <body style='background-color:#ffdead;'>
          <div align='center' style='font-family: Arial;font-size: 14pt;'>
            <p>Instead of saying, <br />\"In this global pandemic?\"</p>
 
            <p>Try saying, <br />\"#{Pandemic.goof}\"</p>
+
+           <p><a href='/' style='color: black;font-family: Arial;font-size: 10pt;'>refresh</a></p>
          </div>
 
          <div align='center' style='margin-top:300px;'>
-           <a href='/api' target='_blank' style='color: black;font-family: Arial;font-size: 12pt;'>Check out our awesome API</a>
+           <a href='/api' target='_blank' style='color: black;font-family: Arial;font-size: 10pt;'>Check out our awesome API</a>
        </body>
      </html>"
   end
