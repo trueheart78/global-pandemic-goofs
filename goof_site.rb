58760564357ddafb1d2f33c4c298d2dcedc1aba1
Josh Mills <josh@trueheart78.com>
2021-02-25 10:29:42 -0500

    New index layout to make life easier.

--- goof_site.rb
+++ goof_site.rb
@@ -4,48 +4,8 @@ require 'lib/pandemic'
 
 class GoofSite < Sinatra::Base
   get '/' do
-    "<!DOCTYPE html>
-     <html>
-       <head>
-         <title>Pandemic Goofs</title>
-         <link rel='shortcut icon' type='image/png' href='icons/microbe.png'/>
-         <style>
-         p, span {
-           font-family: Arial;
-           font-size: 14pt;
-         }
-
-         </style>
-         <script>
-         function copyPandemicGoof() {
-           var r = document.createRange();
-           r.selectNode(document.getElementById('pandemicGoof'));
-           window.getSelection().removeAllRanges();
-           window.getSelection().addRange(r);
-           document.execCommand('copy');
-           window.getSelection().removeAllRanges();
-
-           document.getElementById('goofCopier').innerText = 'Goof Copied!';
-         }
-       </script>
-       </head>
-       <body style='background-color:#ffdead;'>
-         <div align='center' style='font-family: Arial;font-size: 14pt;'>
-           <p>Instead of saying, <br />\"In this global pandemic?\"</p>
-
-           <p>Try saying, <br />\"<span id='pandemicGoof' style='cursor:pointer;' onclick='copyPandemicGoof()'>#{Pandemic.goof}</span>\"</p>
-
-           <p>
-             <a href='#' style='color: black;font-family: Arial;font-size: 10pt;' onclick='copyPandemicGoof()' id='goofCopier'>Copy Goof</a> -
-             <a href='/' style='color: black;font-family: Arial;font-size: 10pt;'>New Goof</a>
-           </p>
-         </div>
-
-         <div align='center' style='margin-top:300px;'>
-           <a href='/api' target='_blank' style='color: black;font-family: Arial;font-size: 10pt;'>Check out our awesome API</a>
-         </div>
-       </body>
-     </html>"
+    @goof = Pandemic.goof
+    erb :index
   end
 
   get '/api' do
