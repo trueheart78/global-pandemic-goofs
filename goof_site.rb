f3ac0bb5fdd69d11ac22d181f2b63e36a3efe68e
Josh <trueheart78@users.noreply.github.com>
2021-03-07 00:11:35 -0500

    Extract GET /status and GET /contents into their own routes.

--- goof_site.rb
+++ goof_site.rb
@@ -41,53 +41,28 @@ class GoofSite < Sinatra::Base
     redirect '/' unless Env.development?
 
     require 'files'
-
-    output = []
-    Files.full_list.each do |file|
-      lines = Files.lines(file)
-      output << "There are #{lines} lines in <a href='/contents/#{file}'>data/#{file}.txt</a>"
+    
+    @output = []
+    Files.full_list.each do |filename|
+      lines = Files.lines(filename)
+      @output << "There are #{lines} lines in <a href='/contents/#{filename}'>data/#{filename}.txt</a>"
     end
 
-    "<!DOCTYPE html>
-    <html>
-      <head>
-        <title>Development Status</title>
-      </head>
-      <body style='font-family: Courier New;'>
-        <div style='margin-left:10px;'>
-          <h4>Development Status</h4>
-          <ul style='padding-left:10px;'>
-            <li>#{output.join("</li>\n            <li>")}</li>
-          </ul>
-        </div>
-      </body>
-    </html>"
+    erb :status
   end
 
   get '/contents/:filename' do
     redirect '/' unless Env.development?
-
-    file = "data/#{params['filename']}.txt"
-    unless File.exist? file
+    
+    @filename = params['filename']
+    file_path = "data/#{@filename}.txt"
+    unless File.exist? file_path
       halt 404, '<div style="font-family: Courier New;">404 - Invalid filename</div>'
     end
 
-    lines = File.readlines(file)
-
-    "<!DOCTYPE html>
-    <html>
-      <head>
-        <title>#{file}</title>
-      </head>
-      <body style='font-family: Courier New;'>
-        <div style='margin-left:10px;'>
-          <h4>#{file} (#{lines.count} entries)</h4>
-          <ol>
-            <li>#{lines.map(&:chomp).join("</li>\n            <li>")}</li>
-          </ol>
-        </div>
-      </body>
-    </html>"
+    @lines = File.readlines(file_path)
+    
+    erb :contents
   end
 
   # catch-all routes
